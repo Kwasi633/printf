2d65abafe3807ac5abe610d980f22def44bd960f
Kwasi633 <ezrakwasi633@gmail.com>
2023-03-28 21:49:34 -0700

    Resolve errors

--- main.h
+++ main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define UNUSED(x) (voi)(x)
+#define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
 
 /* FLAGS */
@@ -40,7 +40,7 @@ struct fmt
 typedef struct fmt fm_t;
 
 int _printf(const char *format, ...);
-int habdle_print(const char *fmt, int *i,
+int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /************* FUNCTIONS ***************/
@@ -67,7 +67,7 @@ int print_hexadecimal(va_list types, char buffer[],
 int print_hexa_upper(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 
-int print_hexa(va_list types, int char map_to[],
+int print_hexa(va_list types, char map_to[],
 char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
 /* Functions to print non printable characters */
@@ -103,8 +103,8 @@ int write_pointer(char buffer[], int ind, int length,
 		int width, int flags, char padd, char extra_c, int padd_start);
 
 int write_unsgnd(int is_negative, int ind,
-		char buffer[],
-		int flags, int width, int precision, int size);
+char buffer[],
+	int flags, int width, int precision, int size);
 
 /************** UTILS *******************/
 int is_printable(char);
@@ -112,6 +112,6 @@ int append_hexa_code(char, char[], int);
 int is_digit(char);
 
 long int convert_size_number(long int num, int size);
-long int convert_size_unsgnd(unsignedvlong int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
 
 #endif /* MAIN_H */
