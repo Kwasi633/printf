126e9a70a6ca5904fee3287fc937f977001b8e83
Kwasi633 <ezrakwasi633@gmail.com>
2023-03-28 21:30:27 -0700

    resolve

--- _printf.c
+++ _printf.c
@@ -26,13 +26,14 @@ int _printf(const char *format, ...)
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			printed _chars++;
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
 		}
 		else
 		{
 			print_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
-			width = get_width(format, &i, &list);
+			width = get_width(format, &i, list);
 			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
