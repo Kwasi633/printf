f863d7e0e324004469f427dbc5a2244db32d5124
Dennis Otchere <sekyeredennis3@gmail.com>
2023-03-30 14:53:52 +0000

    updated files: main.h, functions-1.c, print_handlers.c, and write_handlers.c. Removed file add

--- print_handlers.c
+++ print_handlers.c
@@ -11,11 +11,10 @@
 * @size: Size specifier
 * Return: 1 or 2;
 */
-
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 {
-	int i, unknown_len = 0, printed_chars = -1;
+	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
@@ -26,16 +25,15 @@ int flags, int width, int precision, int size)
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
 			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
-
 	if (fmt_types[i].fmt == '\0')
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
-		unknown_len += write(1, "%%", 1);
+		unknow_len += write(1, "%%", 1);
 		if (fmt[*ind - 1] == ' ')
-			unknown_len += write(1, " ", 1);
+			unknow_len += write(1, " ", 1);
 		else if (width)
-		{	
+		{
 			--(*ind);
 			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
 				--(*ind);
@@ -43,8 +41,8 @@ int flags, int width, int precision, int size)
 				--(*ind);
 			return (1);
 		}
-		unknown_len += write(1, &fmt[*ind], 1);
-		return (unknown_len);
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
 	}
 	return (printed_chars);
 }
